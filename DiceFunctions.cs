8bffd105b2e9cec30bfa8c5b1e4f38772dc0a58b
bybeejac000 <jake.bybee@bybeemail.com>
2025-01-08 20:22:17 -0700

    Add project files.

---
+++ DiceFunctions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dicegame
+{
+    internal class DiceFunctions
+    {
+        int rolls;
+        public DiceFunctions(int temp)
+        {
+            rolls = temp;
+        }
+
+        public void Simulate()
+        {
+            Random random = new Random();
+            int firstDie;
+            int secondDie;
+            int[] NumArray = new int[rolls];
+
+            for (int i = 0; i < rolls; i++)
+            {
+                firstDie = random.Next(1,7);
+                secondDie = random.Next(1,7);
+
+                int sumResult = firstDie + secondDie;
+
+                NumArray[i] = sumResult;
+            }
+
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+
+            foreach(int i in NumArray)
+            {
+                if (counts.ContainsKey(i))
+                {
+                    counts[i]++;
+                }
+
+                else
+                {
+                    counts[i] = 1;
+                }
+            }
+
+            Dictionary<int, int> sortedCounts = counts.OrderBy(keyvalues => keyvalues.Key)
+                .ToDictionary(keyvalues => keyvalues.Key, keyvalues => keyvalues.Value);
+
+            System.Console.WriteLine("Data");
+
+            foreach(var keyValuePair in sortedCounts)
+            {
+                double pctn = ((double)keyValuePair.Value / rolls)*100;
+
+                System.Console.Write($"{keyValuePair.Key} :");
+                for (int i = 0; i < Math.Round(pctn); i++)
+                {
+                    System.Console.Write("*");
+                }
+
+                System.Console.WriteLine($" {keyValuePair.Key}({keyValuePair.Value})");
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Dicegame;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        int rolls;
+
+        Console.WriteLine("How many times do you want to roll?");
+        rolls = int.Parse(Console.ReadLine());
+
+     
+        DiceFunctions df = new DiceFunctions(rolls);
+
+        df.Simulate();
+
+    }
+}
